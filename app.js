c958af0678f9bc5360b8ec0c85c26f1a87fdfa7a
Vasanth Ram <82960398+thevasanthram@users.noreply.github.com>
2022-06-21 15:24:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require('express');
-const https = require('https')
-const res = require('express/lib/response');
 const app = express();
-
-app.use((req, res, next) => {
-  console.log('time:', Date(Date.now()));
-  next();
-});
 
 app.get('/', (req, res) => {
   // console.log('hello world');
-  res.send('hello world' + req.route);
-});
-app.param('vasanth', () => {
-  console.log('understood, this is used for to get parameters from url');
-  next();
-});
-app.get('/home:id', (req, res) => {
-  // console.log('hello world');
-  res.send(`welcome home ${req.params.id}`);
+  res.send('hello from load balancer 1');
 });
 
-app.use(express.static('./folder1'));
-
-app.listen(2000, () => {
-  console.log('app listening on port 2000');
+app.listen(3000, () => {
+  console.log('app listening on port 3000');
 });
